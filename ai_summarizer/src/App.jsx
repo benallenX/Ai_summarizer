796b4dab7c4141f62ebaccc303b762d775fd076c
Ben Allen <benjaminaallenjr@gmail.com>
2023-09-23 20:18:32 -0400

    adding tailwincss and vite setup

---
+++ ai_summarizer/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <h1 className='mt-10 text-center text-4xl text-red-700'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
+
