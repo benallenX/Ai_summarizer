c56a8559487180d9ec8047ef37809b12c0d52283
Ben Allen <benjaminaallenjr@gmail.com>
2023-09-24 17:10:21 -0400

    finished hero section

--- ai_summarizer/src/App.jsx
+++ ai_summarizer/src/App.jsx
@@ -1,11 +1,15 @@
-import{Home,Demo} from './components'
+import{Hero,Demo} from './components'
 import './App.css'
 const App = () => {
   return (
     <main>
-      <h1 className='mt-10 text-center text-4xl text-red-700'>Hello</h1>
-      <Home />
-      <Demo/>
+      <div className='main'>
+        <div className='gradient' />
+     </div>
+      <div className='app'>
+        <Hero />
+        <Demo/>
+      </div>
     </main>
   )
 }
