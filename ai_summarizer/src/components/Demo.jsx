3bf8d8908c07a3aca816e8dc843910a2b4da6ef1
Ben Allen <benjaminaallenjr@gmail.com>
2023-09-24 18:21:28 -0400

    working on demo page

--- ai_summarizer/src/components/Demo.jsx
+++ ai_summarizer/src/components/Demo.jsx
@@ -1,9 +1,34 @@
-
+import React, { useState, useEffect } from 'react'
+import {copy, linkIcon, loader, tick} from '../assets'
 const Demo = () => {
   return (
-    <div>
-      <h1>Demo</h1>
-    </div>
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form className='relative flex justify-center items-center'
+          //onSubmit={ }
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5' />
+          
+          <input
+            type='url'
+            placeholder='Paste the article link'
+           // value={value}
+            onChange={() => ({})}
+            //onKeyDown={ }
+            required
+            className='url_input peer' />
+          <button 
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'>
+            <p>↵</p>n
+            </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
