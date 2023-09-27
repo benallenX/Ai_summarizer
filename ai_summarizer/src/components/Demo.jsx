b4bae68b1d2e731c27dffb2ffe23c01b1136c361
Ben Allen <benjaminaallenjr@gmail.com>
2023-09-27 18:00:48 -0400

    finish demo.js

--- ai_summarizer/src/components/Demo.jsx
+++ ai_summarizer/src/components/Demo.jsx
@@ -41,6 +41,19 @@ const Demo = () => {
       localStorage.setItem('articles', JSON.stringify(updateAllArticles))
     }
   }
+
+  //copy the url and toggle the icon for user feedback
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false),3000)
+  }
+
+  const handleKeyDown = e => {
+    if (e.keyCode === 13) {
+      handleSubmit(e)
+    }
+  }
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
@@ -56,17 +69,68 @@ const Demo = () => {
           <input
             type='url'
             placeholder='Paste the article link'
-           // value={value}
-            onChange={() => ({})}
-            //onKeyDown={ }
+           value={article.url}
+            onChange={(e) => setArticle({...article, url:e.target.value})}
+            onKeyDown={handleKeyDown }
             required
             className='url_input peer' />
           <button 
             type='submit'
             className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'>
-            <p>↵</p>n
+            <p>↵</p>
             </button>
         </form>
+
+        {/* Browse History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.reverse().map((item, index) => (
+            <div 
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img
+                  src={ copied === item.url ? tick : copy}
+                  alt={copied === item.url ? 'tick_icon' : 'copy_icon'}
+                  className='w-[40%] h-[40%] object-contain'
+                />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+              </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Display Result */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+      {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+            <p className='font-inter font-bold text-black-center'>
+              Well, that wasn't supposed to happen...
+              <br />Try again later! 😅😂🤷‍♀️
+              <span className='font-satoshi font-normal text-gray-700'>
+                {error?.data?.error}
+              </span>
+            </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+            </div>
+      </div>
+         ) 
+      )}
+              
       </div>
     </section>
   )
